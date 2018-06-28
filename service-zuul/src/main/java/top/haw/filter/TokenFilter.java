cde7370835d3c2a93c308fd9f8466132e09e19e6
haw <haw358linux@163.com>
2018-06-28 21:50:50 +0800

    为TokenFilter各个方法加上文档注释

--- service-zuul/src/main/java/top/haw/filter/TokenFilter.java
+++ service-zuul/src/main/java/top/haw/filter/TokenFilter.java
@@ -12,23 +12,46 @@ import javax.servlet.http.HttpServletRequest;
  * Created by haw on 18-6-26.
  */
 @Component
-public class MyFilter extends ZuulFilter {
+public class TokenFilter extends ZuulFilter {
 
-    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+    private static Logger log = LoggerFactory.getLogger(TokenFilter.class);
+    /**
+     * 过滤器的类型，它决定过滤器在请求的哪个生命周期中执行。
+     * 这里定义为pre，代表会在请求被路由之前执行。
+     *
+     * @return
+     */
     @Override
     public String filterType() {
         return "pre";
     }
 
+    /**
+     * filter执行顺序，通过数字指定。
+     * 数字越大，优先级越低。
+     *
+     * @return
+     */
     public int filterOrder() {
         return 0;
     }
 
+    /**
+     * 判断该过滤器是否需要被执行。这里我们直接返回了true，因此该过滤器对所有请求都会生效。
+     * 实际运用中我们可以利用该函数来指定过滤器的有效范围。
+     *
+     * @return
+     */
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
+    /**
+     * 过滤器的具体逻辑
+     *
+     * @return
+     */
     @Override
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
